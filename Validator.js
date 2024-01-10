163bca201a969a3573c361005f6a77a38635c1f2
SharmilaMohanraj <106313092+SharmilaMohanraj@users.noreply.github.com>
2024-01-10 17:24:21 +0530

    Add files via upload

---
+++ Validator.js
@@ -0,0 +1,51 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Validator {
+    isAlphaNum(pValue) {
+        const exp = new RegExp(/^([a-zA-Z0-9 _-]+)$/);
+        if (exp.test(pValue)) {
+            return true;
+        }
+        return false;
+    }
+    isOnlyNumber(pValue) {
+        const exp = new RegExp(/^\d+$/);
+        if (exp.test(pValue)) {
+            return true;
+        }
+        return false;
+    }
+    isNotUndefinedAndNull(pValue) {
+        let value = pValue;
+        if (typeof value === 'string') {
+            value = pValue.trim();
+        }
+        if (value !== undefined && value !== null && value !== '' && value !== 'null' && value !== 'undefined') {
+            return true;
+        }
+        return false;
+    }
+    isNullOrUndefined(pValue) {
+        if (pValue === undefined || pValue === null || (typeof pValue === 'string' && pValue.trim() === '') || pValue === 'null' || pValue === 'undefined') {
+            return true;
+        }
+        return false;
+    }
+    isArrayEmpty(pValue) {
+        if (!pValue || !Array.isArray(pValue) || pValue.length === 0) {
+            return true;
+        }
+        return false;
+    }
+    isJson(str) {
+        try {
+            JSON.parse(str);
+        }
+        catch (error) {
+            return false;
+        }
+        return true;
+    }
+}
+exports.default = new Validator();
+//# sourceMappingURL=Validator.js.map
