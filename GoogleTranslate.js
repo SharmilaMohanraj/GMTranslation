56881c3a9c8f8ddfb53060cde20efe7c0c5165d8
SharmilaMohanraj <106313092+SharmilaMohanraj@users.noreply.github.com>
2024-01-10 17:23:40 +0530

    Add files via upload

--- GoogleTranslate.js
+++ GoogleTranslate.js
@@ -1,52 +1,54 @@
-import Validator from "./Validator";
-import * as translate from "@google-cloud/translate";
-
-const Translate  = translate.v2.Translate;
-const TranslationServiceClient  = translate.TranslationServiceClient;
-
-export class GoogleTranslate {
-
-    public async translationLocale(pText: any, pTarget: string): Promise<any> {
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GoogleTranslate = void 0;
+const common_1 = require("@nestjs/common");
+const Validator_1 = require("./Validator");
+const translate = require("@google-cloud/translate");
+const Translate = translate.v2.Translate;
+const TranslationServiceClient = translate.TranslationServiceClient;
+let GoogleTranslate = class GoogleTranslate {
+    async translationLocale(pText, pTarget) {
         try {
             console.log('In translationLocale method');
             let configJson = global.appConfig["GM_CONTENT_GOOGLE_TRANSLATE_CREDENTIALS"];
-            console.log(`configJson is: ${configJson}`)
+            console.log(`configJson is: ${configJson}`);
             configJson = JSON.parse(configJson);
             const translate = new Translate({ credentials: configJson });
-            const translatePromises: any = [];
-            let translations: any = [];
-            let promiseResponse: any;
-            if (Array.isArray(pText) && !Validator.isArrayEmpty(pText) && pText.length > 100) {
+            const translatePromises = [];
+            let translations = [];
+            let promiseResponse;
+            if (Array.isArray(pText) && !Validator_1.default.isArrayEmpty(pText) && pText.length > 100) {
                 for (let i = 0, j = pText.length; i < j; i = i + 100) {
                     const chunkTexts = pText.slice(i, i + 100);
                     translatePromises.push(translate.translate(chunkTexts, pTarget));
                 }
                 promiseResponse = await Promise.all(translatePromises);
-                if (Array.isArray(promiseResponse) && !Validator.isArrayEmpty(promiseResponse)) {
+                if (Array.isArray(promiseResponse) && !Validator_1.default.isArrayEmpty(promiseResponse)) {
                     for (const pItem of promiseResponse) {
                         translations.push(...pItem[0]);
                     }
                 }
-            } else {
+            }
+            else {
                 translatePromises.push(translate.translate(pText, pTarget));
                 promiseResponse = await Promise.all(translatePromises);
                 [translations] = promiseResponse[0];
-
-                // translatePromises.push(this.translateLocaleAsDummy(pText, pTarget));
-                // promiseResponse = await Promise.all(translatePromises);
-                // [translations] = promiseResponse;
             }
-            translations = (Validator.isNotUndefinedAndNull(translations) && Array.isArray(translations)) ? translations : [translations];
+            translations = (Validator_1.default.isNotUndefinedAndNull(translations) && Array.isArray(translations)) ? translations : [translations];
             return ({ error: null, data: translations, locale: pTarget });
-        } catch (error) {
+        }
+        catch (error) {
             console.log("GoogleTranslate-> translateLocale-> err", error);
             return { error, data: null, locale: pTarget };
         }
     }
-
-    // Create a glossary resource
-    public async createGlossary(pLanguageCodes: any, pGlossaryFileName: string): Promise<any> {
-        // Construct glossary
+    async createGlossary(pLanguageCodes, pGlossaryFileName) {
         let configJson = global.appConfig["GM_CONTENT_GOOGLE_TRANSLATE_CREDENTIALS"];
         configJson = JSON.parse(configJson);
         const projectId = configJson.project_id;
@@ -65,21 +67,15 @@ export class GoogleTranslate {
             },
             name: `projects/${projectId}/locations/${location}/glossaries/${glossaryId}`,
         };
-
-        // Create glossary using a long-running operation
         const [operation] = await translate.createGlossary({
             parent: `projects/${configJson.project_id}/locations/${location}`,
             glossary: glossary,
         });
-        // Wait for the operation to complete
         await operation.promise();
         console.log("Created glossary:");
         console.log(`InputUri ${glossary.inputConfig.gcsSource.inputUri}`);
     }
-
-
-    // Instantiates a client
-    public async translateTextWithGlossary(pText: any, pTarget: string): Promise<any> {
+    async translateTextWithGlossary(pText, pTarget) {
         let configJson = global.appConfig["GM_CONTENT_GOOGLE_TRANSLATE_CREDENTIALS"];
         configJson = JSON.parse(configJson);
         const projectId = configJson.project_id;
@@ -92,45 +88,42 @@ export class GoogleTranslate {
         const request = {
             parent: `projects/${projectId}/locations/${location}`,
             contents: null,
-            mimeType: 'text/plain', // mime types: text/plain, text/html
+            mimeType: 'text/plain',
             sourceLanguageCode: 'en',
             targetLanguageCode: pTarget,
             glossaryConfig: glossaryConfig,
         };
-        // const request = {};
         const textArray = Array.isArray(pText) ? pText : [pText];
-        // Run request
-        // console.log('textArray', JSON.stringify(textArray));
         console.log('translateTextWithGlossary:textArray:', textArray.length);
-        const translations: any = [];
-        if (Array.isArray(textArray) && !Validator.isArrayEmpty(textArray) && textArray.length > 0) {
+        const translations = [];
+        if (Array.isArray(textArray) && !Validator_1.default.isArrayEmpty(textArray) && textArray.length > 0) {
             console.log('translateTextWithGlossary:textArray:first item before translation:', textArray[0]);
             console.log('translateTextWithGlossary:textArray:last item before translation:', textArray[textArray.length - 1]);
             for (let i = 0, j = textArray.length; i < j; i = i + 100) {
                 const chunkTexts = textArray.slice(i, i + 100);
                 try {
                     console.log('translateTextWithGlossary:translateText:chunkTexts:sent:', chunkTexts.length);
-                    const translationResult: any = await translate.translateText({ ...request, contents: chunkTexts });
-                    // const translationResult: any = await this.translateGlossaryAsDummy({ ...request, contents: chunkTexts }, pTarget);
+                    const translationResult = await translate.translateText({ ...request, contents: chunkTexts });
                     if (translationResult && translationResult[0] && translationResult[0].glossaryTranslations) {
                         console.log('translateTextWithGlossary:translateText:chunkTexts:received:', translationResult[0].glossaryTranslations.length);
-                        translationResult[0].glossaryTranslations.forEach((item: any) => {
+                        translationResult[0].glossaryTranslations.forEach((item) => {
                             translations.push(item.translatedText);
                         });
-                    } else {
+                    }
+                    else {
                         console.log('translateTextWithGlossary:translateText:NULL:');
                         chunkTexts.forEach((item) => {
                             translations.push(item);
                         });
                     }
-                } catch (tError) {
+                }
+                catch (tError) {
                     console.log('translateTextWithGlossary:translateText:error:', tError);
                     chunkTexts.forEach((item) => {
                         translations.push(item);
                     });
                 }
             }
-            // console.log('translateTextWithGlossary:translations:', JSON.stringify(translations));
             console.log('translateTextWithGlossary:translations:', translations.length);
             if (translations.length > 0) {
                 console.log('translateTextWithGlossary:textArray:first item after translation:', translations[0]);
@@ -139,24 +132,27 @@ export class GoogleTranslate {
         }
         return { error: null, data: translations, locale: pTarget };
     }
-
-    public translateLocaleAsDummy(pText: any, pTo: string): any {
-        return new Promise((resolve: any) => {
-            const items: any = [];
-            pText.forEach((item: any) => items.push(`${item}__${pTo}`));
+    translateLocaleAsDummy(pText, pTo) {
+        return new Promise((resolve) => {
+            const items = [];
+            pText.forEach((item) => items.push(`${item}__${pTo}`));
             resolve(items);
         });
     }
-
-    public translateGlossaryAsDummy(request: any, pLocale: string): any {
-        const data : any = [];
-        return new Promise((resolve: any) => {
+    translateGlossaryAsDummy(request, pLocale) {
+        const data = [];
+        return new Promise((resolve) => {
             setTimeout(() => {
-                const glossaryTranslations: any = [];
-                request.contents.forEach((content: any) => glossaryTranslations.push({ translatedText: `${content}_${pLocale}` }));
+                const glossaryTranslations = [];
+                request.contents.forEach((content) => glossaryTranslations.push({ translatedText: `${content}_${pLocale}` }));
                 data.push({ glossaryTranslations });
                 resolve(data);
             }, 100);
         });
     }
-}
+};
+GoogleTranslate = __decorate([
+    (0, common_1.Injectable)()
+], GoogleTranslate);
+exports.GoogleTranslate = GoogleTranslate;
+//# sourceMappingURL=GoogleTranslate.js.map
